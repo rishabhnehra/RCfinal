73d056b54836326dee8f7375ca360c1e9ff410e8
Rishabh Nehra <rishabhnehrapersonal@gmail.com>
2016-06-22 00:38:06 +0530

    Update settings.py

--- rconsultants/settings.py
+++ rconsultants/settings.py
@@ -23,7 +23,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '2+wkex@!gs3d&c2fwv34uv_)7x($go5uul9@vd(lz%!q7h3i_k'
 
-ALLOWED_HOSTS = ['rishabhconsultants.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
